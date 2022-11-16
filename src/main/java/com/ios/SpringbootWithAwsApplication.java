ff83cf4b5da054f8a1f54c7a5e920e20d8fd66d3
samgithub400 <shaikhtohit95@gmail.com>
2022-11-16 20:07:06 +0530

    First Commit 1

---
+++ src/main/java/com/ios/SpringbootWithAwsApplication.java
@@ -0,0 +1,14 @@
+package com.ios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAwsApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome TO The Spring Boot App With AWS..!");
+		SpringApplication.run(SpringbootWithAwsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ios/model/User.java
@@ -0,0 +1,35 @@
+package com.ios.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "UserInfo")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "UserId")
+	private long userId;
+	
+	@Column(name = "FirstName")
+	private String firstName;
+	
+	@Column(name = "LastName")
+	private String lastName;
+	
+	@Column(name = "Email")
+	private String email;
+}
